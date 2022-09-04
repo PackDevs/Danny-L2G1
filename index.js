7f2df13919e2bea362d61b726e4f4a7b725d7e2b
Dannydaniel1231 <ndaniel4040@gmail.com>
2022-09-04 11:56:01 +0200

    commit on index.js push to portfolio

---
+++ index.js
@@ -0,0 +1,24 @@
+const bod =document.querySelector("body");
+console.log(bod)
+let body=document.getElementById("body")
+console.log(body)
+ let cont=document.getElementById("navaba");
+//  console.log(cont)
+let lft=document.getElementsByClassName("left");
+ let rgt=document.getElementsByClassName("right");
+
+body.addEventListener("click",function(e){
+    
+ })
+
+
+// let bod=document.getElementById("but");
+// console.log("bod");
+// let but=document.getElementById("but");
+// let lft=document.getElementById("lft");
+// let rgt=document.getElementById("rgt");
+
+
+// but.addEventListener("click",function(e){
+    
+// })
